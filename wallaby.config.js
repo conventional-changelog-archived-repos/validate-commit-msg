d924a34f2692513f86a714505b5ff5ec68c5230a
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2017-01-22 23:25:29 +0100

    feat(config): config through rc file, has precedence over package.json (#62)

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   return {
-    files: ['lib/**/*.js', 'package.json'],
+    files: ['lib/**/*.js', 'package.json', '.vcmrc'],
     tests: ['test/**/*.js'],
     env: {
       type: 'node'
