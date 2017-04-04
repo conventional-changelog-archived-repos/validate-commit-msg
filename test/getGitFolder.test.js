e3a81f8298427b94be01e7ea305505ee6005cec8
standy <mostovoyav@gmail.com>
2017-04-04 13:11:13 +0300

    fix(lib): getting .git from parent dir (#74)
    * fix(lib): getting .git from parent dir
    
    * docs(contributors): add myself to contributors
    
    * docs(contributors): fix myself to use code instead of bug

--- test/getGitFolder.test.js
+++ test/getGitFolder.test.js
@@ -3,6 +3,8 @@
 var sinon = require('sinon');
 var expect = require('chai').expect;
 var fs = require('fs');
+var resolve = require('path').resolve;
+var findParentDir = require('find-parent-dir');
 var getGitFolder = require('../lib/getGitFolder');
 
 describe('handle .git as folder', function() {
@@ -20,7 +22,7 @@ describe('handle .git as folder', function() {
   });
 
   it('should return ./.git when .git is a directory', function() {
-    expect(getGitFolder()).to.be.equal('./.git');
+    expect(getGitFolder()).to.be.equal(resolve('.git'));
   });
 
   after(function() {
@@ -38,7 +40,7 @@ describe('handle .git as file', function() {
       //Ensure that we say ./.git is a file, but ../../actual-folder is folder
       return {
         isDirectory: function() {
-          return location != './.git';
+          return location != resolve('.git');
         }
       }
     });
@@ -48,7 +50,7 @@ describe('handle .git as file', function() {
   });
 
   it('should load gitdir from .git file', function() {
-    expect(getGitFolder()).to.be.equal('../../actual-folder');
+    expect(getGitFolder()).to.be.equal(resolve('../../actual-folder'));
   });
 
   after(function() {
@@ -60,36 +62,39 @@ describe('handle .git as file', function() {
 
 describe('handle .git does not exist', function() {
   before(function() {
-    sinon.stub(fs, "existsSync", function() {
-      return false;
+    sinon.stub(findParentDir, 'sync', function() {
+      return null;
     })
   });
 
   it('should throw error when ./.git is missing', function() {
-    expect(getGitFolder).to.throw('Cannot find file ./.git');
+    expect(getGitFolder).to.throw('Cannot find .git folder');
   });
 
   after(function() {
-    fs.existsSync.restore();
+    findParentDir.sync.restore();
   });
 });
 
 describe('handle .git gitdir: folder does not exist', function() {
   before(function() {
+    sinon.stub(findParentDir, 'sync', function() {
+      return resolve('.');
+    });
     sinon.stub(fs, "existsSync", function(dir) {
-      return './.git' == dir;
+      return dir == resolve('.git');
     });
     sinon.stub(fs, "lstatSync", function(location) {
       //Ensure that we say ./.git is a file, but ../../actual-folder is folder
       return {
         isDirectory: function() {
-          return location != './.git';
+          return location != resolve('.git');
         }
       }
     });
     sinon.stub(fs, 'readFileSync', function() {
       return 'gitdir: ../../actual-folder';
-    })
+    });
   });
 
   it('should throw error when ./.git is missing', function() {
@@ -97,6 +102,7 @@ describe('handle .git gitdir: folder does not exist', function() {
   });
 
   after(function() {
+    findParentDir.sync.restore();
     fs.existsSync.restore();
     fs.lstatSync.restore();
     fs.readFileSync.restore();
